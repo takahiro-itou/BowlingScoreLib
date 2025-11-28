b2d33d426a5d04a70d38d523fc08ddb9acb7abe3
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-11-28 20:49:34 +0900

    ヘッダファイルを新規作成

---
+++ Include/BowlingScore/Common/BowlingTypes.h
@@ -0,0 +1,39 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                      ---   Bowling Score.   ---                      **
+**                                                                      **
+**          Copyright (C), 2025-2025, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      プロジェクトの設定。
+**
+**      @file       Common/BowlingTypes.h
+**/
+
+#if !defined( BOWLINGSCORE_COMMON_INCLUDED_BOWLING_TYPES_H )
+#    define   BOWLINGSCORE_COMMON_INCLUDED_BOWLING_TYPES_H
+
+#if !defined( BOWLINGSCORE_COMMON_INCLUDED_BOWLING_SETTINGS_H )
+#    include    "BowlingSettings.h"
+#endif
+
+#if !defined( BOWLINGSCORE_SYS_INCLUDED_CSTDINT )
+#    include    <cstdint>
+#    define   BOWLINGSCORE_SYS_INCLUDED_CSTDINT
+#endif
+
+
+BOWLINGSCORE_NAMESPACE_BEGIN
+
+
+BOWLINGSCORE_NAMESPACE_END
+
+#endif
